1f4e5b36c066252eabb27fe41a36776f3a546a28
vaakavak <vaakavak@gmail.com>
2025-05-09 16:11:24 +0400

    Initial commit

---
+++ Day 1 - WeSplit/WeSplit/WeSplitTests/WeSplitTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitTests.swift
+//  WeSplitTests
+//
+//  Created by Евгений Шилов on 09.05.2025.
+//
+
+import Testing
+@testable import WeSplit
+
+struct WeSplitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
